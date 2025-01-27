f7071ed520ebaea543db543545554af656b16827
Samgar Aidarhanov Pautrat <aidarhans@bxscience.edu>
2025-01-27 10:20:47 -0500

    Update Projects.jsx

--- portfolio/src/components/Projects.jsx
+++ portfolio/src/components/Projects.jsx
@@ -6,18 +6,32 @@ const Projects = () => {
     const projectList = [
       { name: "Stock_trading_backtesting_react_page", 
         description: "Users will be able to select a certain trading strategy and generate real api data on the desired stock name. Users will be able to generate statistics of the results of a strategy users will be able to choose from and see heatmaps as well as other visual analytics", 
-        image: project1 },
-      { name: "Language Processing Algorithm", description: "Redesigned the user flow for a pet service app.", image: project2 },
-      { name: "Heart Attack Data Analysis", description: "data analysis project on a gold star kaggle dataset for Cornell University's INFO 2950 class", image: project3 },
+        image: project1,
+        url: "https://sampautrat.netlify.app"
+      },
+      { name: "Language Processing Algorithm", 
+        description: "Redesigned the user flow for a pet service app.", 
+        image: project2,
+        url: "placeholder.com"
+      },
+      { name: "Heart Attack Data Analysis", 
+        description: "data analysis project on a gold star kaggle dataset for Cornell University's INFO 2950 class", 
+        image: project3,
+        url: "placeholder.com"
+      },
     ];
-  
+    const handleCardClick = (url) => {
+      window.location.href = url;
+    };
     return (
       <section id="projects" className="projects">
         <h2>My Projects</h2>
         <div className="project-cards">
           {projectList.map((project, index) => (
-            <div className="project-card" key={index}>
-              <img src={project.image} alt={project.name} className="project-image" href="sampautrat.netlify.app" />
+            <div className="project-card" 
+            key={index}
+            onClick={() => handleCardClick(project.url)}>
+              <img src={project.image} alt={project.name} className="project-image" />
               <h3>{project.name}</h3>
               <p>{project.description}</p>
             </div>
