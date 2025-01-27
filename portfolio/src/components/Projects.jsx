ee8af06fa14ebf8b85b1db1a6e81ec503d72c503
Samgar Aidarhanov Pautrat <aidarhans@bxscience.edu>
2025-01-27 14:39:34 -0500

    testing links

--- portfolio/src/components/Projects.jsx
+++ portfolio/src/components/Projects.jsx
@@ -7,7 +7,7 @@ const Projects = () => {
       { name: "Stock_trading_backtesting_react_page", 
         description: "Users will be able to select a certain trading strategy and generate real api data on the desired stock name. Users will be able to generate statistics of the results of a strategy users will be able to choose from and see heatmaps as well as other visual analytics", 
         image: project1,
-        url: "https://sampautrat.netlify.app"
+        url: "https://trading-backtesting.netlify.app/"
       },
       { name: "Language Processing Algorithm", 
         description: "Redesigned the user flow for a pet service app.", 
